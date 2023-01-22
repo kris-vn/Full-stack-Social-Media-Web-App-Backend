32bd7e067ef54c295d3fa8b151f1f61bb82b2846
Kristofar Vasilev <v.kristofar@gmail.com>
2023-01-22 21:58:26 +0200

    Deleted unused/commented coded

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg')
 const pool = new Pool()
-const fs = require('fs')
 
 
 exports.uploadGif = (req, res, next) => {
 
   const url = req.protocol + '://' + req.get('host')
-  // const { title, image_url, user_id } = req.body.post
 
   const post = {
     title: req.body.title,
@@ -30,7 +28,6 @@ exports.uploadGif = (req, res, next) => {
 exports.modifyGif = (req, res, next) => {
   const id = parseInt(req.params.id)
   const { title, users_read, user_id } = req.body
-  // console.log({ 'id': req.auth.userId })
   let post = ''
 
 
@@ -41,37 +38,6 @@ exports.modifyGif = (req, res, next) => {
       throw error
     }
     const userId = results.rows[0].user_id
-
-    // if (userId !== req.auth.userId) {
-    //   console.log(req.auth.userId, userId, 'Request not authorized!')
-    //   return res.status(401).json({
-    //     error: new Error('Request not authorized!')
-    //   });
-    // }
-
-    // if (req.file) {
-    //   let data = JSON.parse(req.body.post)
-    //   const originalFilename = results.rows[0].image_url.split('/images/')[1]
-    //   // fs.unlink('images/' + originalFilename, () => {
-    //   //   console.log(data)
-    //   //   console.log(originalFilename)
-    //   //   console.log('Removed old image file from database')
-    //   // })
-
-    //   const url = req.protocol + '://' + req.get('host')
-    //   // const { title, image_url, user_id } = req.body.post
-    //   post = {
-    //     title: data.title,
-    //     image_url: url + '/images/' + req.file.filename,
-    //     user_id: data.user_id
-    //   }
-    // } else {
-    //   post = {
-    //     title: req.body.title,
-    //     image_url: req.body.image_url,
-    //     user_id: req.body.user_id
-    //   }
-    // }
 
     post = {
       users_read: req.body.users_read,
@@ -99,7 +65,6 @@ exports.deleteOneGif = (req, res, next) => {
 
   pool.query('SELECT * FROM posts WHERE post_id = $1', [id], (error, results) => {
     const userId = results.rows[0].user_id
-    // const filename = results.rows[0].image_url.split('/images/')[1]
 
     if (error) {
       console.log(error)
@@ -120,19 +85,6 @@ exports.deleteOneGif = (req, res, next) => {
       console.log('Post successfully deleted.')
       return res.status(200).send({ "message": 'Post succesfully deleted' })
     })
-
-
-    // fs.unlink('images/' + filename, () => {
-    //   pool.query('DELETE FROM posts WHERE post_id = $1', [id], (error, results) => {
-    //     if (error) {
-    //       throw error
-    //     }
-    //     return res.status(200).send('Gif successfully deleted.')
-    //   })
-    // })
-
-    // console.log(results)
-    // res.status(200).json(results.rows)
   })
 
 }
--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,14 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const User = require('../models/User')
 const { Pool } = require('pg')
 const res = require('express/lib/response')
 const { user } = require('pg/lib/defaults')
 const pool = new Pool()
 
 
-
-// exports.addNewUser = (req, res, next) => {
-//     const {username, password, email, bio} = req.body
-//     const user = new User({username, password, email, bio})
-//     user.createUser()
-//     .then(() => {
-//         res.status(201).json({
-//             message: 'User created successfully!'
-//         })
-//       } 
-//     ).catch(
-//         (error) => {
-//             const errorToThrow = new Error();
-//             switch (error?.code) {
-//                 case '23505':
-//                     errorToThrow.message = 'User already exists';
-//                     errorToThrow.statusCode = 403;
-//                     break;
-//                 default:
-//                     errorToThrow.statusCode = 500;
-//             }
-//             //pass error to next()
-//             next(errorToThrow);
-//         }
-//     )
-// }
-
-
 exports.signup = (req, res) => {
   bcrypt.hash(req.body.password, 10).then(
     (hash) => {
-      // const {username, email, password} = req.body
 
       const user = {
         username: req.body.username,
@@ -125,13 +95,6 @@ exports.getOneUser = (req, res, next) => {
 
 
 
-
-
-
-
-
-
-
 exports.deleteUser = (request, response) => {
   const id = parseInt(request.params.id)
 
@@ -142,22 +105,6 @@ exports.deleteUser = (request, response) => {
     response.status(200).send({ "message": "User successfully deleted." })
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
